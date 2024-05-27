8a2b3c7ec35585f8d101f3135dbcaca678a74dca
Jijiluzana225 <jiji.luzana@gmail.com>
2024-05-27 09:10:46 +0800

    Update settings.py

--- lugawan_halin/settings.py
+++ lugawan_halin/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-8_dblv*188-c=*vex(uuzx*y@vm7*+5+^%kofiwtaq8wre!8in'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
